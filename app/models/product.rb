60752bed45498bab65a7877cf5a5d7d416f68a1e
old-programmer <ybauer53@gmail.com>
2017-07-26 20:53:56 +0300

    Refactoring

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 
-	validates :title, :description, :image_url, presence: true
+	validates :title, :discription, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
 
