94d0d0caf37a47dbff5408244ce3bce90ca7c9ee
old-programmer <ybauer53@gmail.com>
2017-07-28 11:39:28 +0300

    Add Cart && Button: Add to Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 	validates :title, :discription, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -10,4 +12,15 @@ class Product < ApplicationRecord
 						 # must be a URL for GIF, JPG or PNG image.'
 	}
 
+	private
+
+	# убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'существуют товарные позиции')
+		return false
+	end
+end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Time::DATE_FORMATS[:ru_datetime] = "%Y.%m.%d, %k:%M:%S"
+  Time::DATE_FORMATS[:ru_datetime] = "%Y.%m.%d  %k:%M"
 end
