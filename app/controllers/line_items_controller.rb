847875a86583f855cb3f5e2aaef519f35ce3214f
old-programmer <ybauer53@gmail.com>
2017-07-28 18:19:27 +0300

    Add Flash, clear Cart, Total Price

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,10 +28,9 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
