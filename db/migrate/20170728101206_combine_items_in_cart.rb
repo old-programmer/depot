6792e652571a599eb710285bd7c7f101a7862d6e
old-programmer <ybauer53@gmail.com>
2017-07-28 14:37:05 +0300

    Add method Down in migrate

--- db/migrate/20170728101206_combine_items_in_cart.rb
+++ db/migrate/20170728101206_combine_items_in_cart.rb
@@ -17,4 +17,17 @@ class CombineItemsInCart < ActiveRecord::Migration[5.1]
 		end
 	end
 
+	def down
+		# разбиение записей с quantity>1 на несколько записей
+		LineItem.where("quantity>1").each do |line_item|
+			# add individual items
+			line_item.quantity.times do
+				LineItem.create cart_id: line_item.cart_id,
+				product_id: line_item.product_id, quantity: 1
+			end
+			# удаление исходной записи
+			line_item.destroy
+		end
+	end
+
 end
